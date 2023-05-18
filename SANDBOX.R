05c6195e16de701e66d333dce38bd35983f2a502
Del'riaan <121366351+delriaan@users.noreply.github.com>
2023-05-17 22:05:04 -0400

    Introducing NDC lookup data

--- SANDBOX.R
+++ SANDBOX.R
@@ -1,1 +1,16 @@
+api_data$NDC |> unique() |> sample(100) |> stri_length() |> table()
+api_data$NDC |> unique() |> sample(100) |> stri_sub(length = 1) |> table()
 
+replicate(
+  n = 30
+  , { inspect$product_ndc |> unique() |> sample(100) |> stri_replace_all_regex("-", "", vectorise_all = FALSE) |> 
+      stri_length() |> table() |> t()
+    })
+
+inspect$product_ndc |> unique() |> sample(100) |> stri_replace_all_regex("-", "", vectorise_all = FALSE) |> stri_sub(length = 1) |> table()
+
+modify_at(api_data, "NDC", \(x) stri_pad_left(x, width = 8, pad = "0"))[
+  inspect[1:10] |> modify_at("product_ndc", \(x) stri_replace_all_regex(x, "-", "", vectorise_all = FALSE))
+  , on = "NDC==product_ndc"
+  , nomatch = NULL
+  ]
