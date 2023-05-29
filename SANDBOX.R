aba73c6861febdb475097c013dce002ca2c4a15c
Chionesu <sapienti.Dei@outlook.com>
2023-05-29 00:50:23 -0400

    Periodic Sync
    1. Improve disk caching logic
    2. Added new section to explore new metrics
    
    As the intent is exploration more than modeling, I may bring this to a close soon.

--- SANDBOX.R
+++ SANDBOX.R
@@ -1,97 +1,20 @@
-# NDC Augmentation ----
+.tmp_obj <- get.smart("drugs")$use(identifier, metrics, retain = c(route, sched), subset = days_to_market >= 0) |> 
+  setattr("class", c("data.table", "data.frame")) |>
+  setkey(route, alt_ndc, days_to_market, on_market_age) |>
+  modify_at(c("days_to_market", "on_market_age"), as.numeric) |>
+  modify_at("dea_schedule", \(x) modify_if(x, is.na, \(i) "~") |> factor()) 
 
-plan(list(tweak(multisession, workers = 5), callr))
-# future_walk(1:5, \(x) book.of.workflow::load_unloaded(magrittr, furrr, future, data.table))
+library(future.callr)
+plan(callr)
 
-# NDC format: lengths (excluding hyphens)
-ndc_lengths %<-% { list(api_NDC = api_data$ndc, openFDA_NDC = openFDA_ndc.combined$product_ndc) |>
+inspect <- replicate(30, dplyr::slice_sample(.tmp_obj, prop = 0.1), simplify = FALSE) |>
   future_map(\(x){ 
-    replicate(
-      n = 30
-      , expr = sample(x, 100) |>
-                stringi::stri_replace_all_regex("[-]|^([0]+)", "", vectorise_all = FALSE) |> 
-                stringi::stri_length() |> 
-                outer(rlang::set_names(7:11), `==`) |> 
-                colSums() %>% 
-                magrittr::divide_by(sum(.))
-      , simplify = FALSE
-      ) |>
-    purrr::reduce(rbind) |>
-    colMeans() |>
-    as.list()
-  }, .options = furrr_options(scheduling = Inf, seed = TRUE)) |>
-  rbindlist(idcol = "source") 
-} %lazy% TRUE %seed% TRUE %packages% c("magrittr", "furrr", "future", "data.table")
+    .tmp_obj; 
+    stats::manova(formula = cbind(days_to_market, on_market_age) ~ route*dea_schedule, data = x) 
+    }, .options = furrr_options(scheduling = Inf, seed = TRUE, packages = c("data.table"))) 
 
-# NDC format: first character/digit
-ndc_first_digit %<-% { list(api_NDC = api_data$ndc, openFDA_NDC = openFDA_ndc.combined$product_ndc) |>
-    future_map(\(x){ 
-      replicate(
-        n = 30
-        , expr = sample(x, 100) |>
-                  stringi::stri_sub(length = 1) |> 
-                  outer(rlang::set_names(0:9), `==`) |> 
-                  colSums() %>% 
-                  divide_by(sum(.))
-        , simplify = FALSE
-        ) |>
-      purrr::reduce(rbind) |>
-      colMeans() |>
-      as.list()
-    }, .options = furrr_options(scheduling = Inf, seed = TRUE)) |>
-    rbindlist(idcol = "source")
-} %lazy% TRUE %seed% TRUE %packages% c("magrittr", "furrr", "future", "data.table")
-
-# NDC format: last character/digit
-ndc_last_digit %<-% { list(api_NDC = api_data$ndc, openFDA_NDC = openFDA_ndc.combined$product_ndc) |>
-    future_map(\(x){ 
-      replicate(
-        n = 30
-        , expr = sample(x, 100) %>%
-                  stringi::stri_sub(from = stringi::stri_length(.), length = 1) |> 
-                  outer(rlang::set_names(0:9), `==`) |> 
-                  colSums() %>% 
-                  divide_by(sum(.))
-        , simplify = FALSE
-        ) |>
-      purrr::reduce(rbind) |>
-      colMeans() |>
-      as.list()
-    }, .options = furrr_options(scheduling = Inf, seed = TRUE)) |>
-    rbindlist(idcol = "source")
-} %lazy% TRUE %seed% TRUE %packages% c("magrittr", "furrr", "future", "data.table")
-
-# NDC format: consecutive zeros
-ndc_zero_seq %<-% { list(api_NDC = api_data$ndc, openFDA_NDC = openFDA_ndc.combined$product_ndc) |>
-    future_map(\(x){ 
-      replicate(
-        n = 30
-        , expr = sample(x, 100) |>
-                  stringi::stri_extract_first_regex("0{1,11}") |> 
-                  stringi::stri_length() |>
-                  purrr::modify_if(\(x) is.na(x)||rlang::is_empty(x), \(x) 0) |>
-                  outer(rlang::set_names(0:5), `==`) |> 
-                  colSums() %>% 
-                  divide_by(sum(.))
-        , simplify = FALSE
-        ) |>
-      purrr::reduce(rbind) |>
-      colMeans() |>
-      as.list()
-    }, .options = furrr_options(scheduling = Inf, seed = TRUE)) |>
-    rbindlist(idcol = "source")
-} %lazy% TRUE %seed% TRUE %packages% c("magrittr", "furrr", "future", "data.table")
-
-invisible(value(.GlobalEnv))
-
-mget(ls(pattern = "^ndc.+(f|l|z)"))
-
-plan(sequential)
-
-
-# Drug Events ----
-
-
-# ----
-master_drug_data[ndc_events, on = c("alt_ndc", "fda_application_number")]
-.tmp_obj
+inspect |>
+  map(broom::tidy) |>
+  rbindlist(idcol = "sample_id") |>
+  define(.SD[!grepl("Resid", term)] |> setorder(term, p.value)) |>
+  print()
--- setup.R
+++ setup.R
@@ -15,6 +15,7 @@ if (!"book.of.workflow" %in% dir(sprintf("%s/library", R.home()))){
   remotes::install_github("delriaan/book.of.features", subdir = "pkg")
   remotes::install_github("delriaan/book.of.workflow", subdir = "pkg")
   remotes::install_github("delriaan/architect", subdir = "pkg")
+  remotes::install_github("delriaan/smart.data", subdir = "pkg")
 }
 
 library(book.of.workflow)
@@ -30,11 +31,57 @@ assign("urls", { list(
     data = list(
       `Data Dictionary` = c("https://www.medicaid.gov/medicaid-chip-program-information/by-topics/prescription-drugs/downloads/recordspecficationanddefinitions.pdf", " (Medicaid.gov)")
       , `MDRP Data` = c("https://download.medicaid.gov/data/drugproducts1q_2023.csv", "")
-      , `openFDA Drug Data` = c("https://download.open.fda.gov/drug/ndc/drug-ndc-0001-of-0001.json.zip", " (used to augment MDRP data)")
-      ) |> 
-      purrr::imap(\(x, y) htmltools::tags$li(htmltools::tags$a(href = x[1], y), x[2])) |> htmltools::tags$ol()
-    , git_libs = purrr::map(globalenv()$params$git_libs, \(x) htmltools::tags$li(htmltools::tags$a(title = paste0("delriaan/", x), x))) |> list() |> htmltools::tags$ul()
-    )
-}, envir = as.environment("nb_env"))
+      , `openFDA Drug Data` = c("https://download.open.fda.gov/drug/ndc/drug-ndc-0001-of-0001.json.zip", " (used to augment MDRP data)")) |> 
+      purrr::imap(\(x, y) htmltools::tags$li(htmltools::tags$a(href = x[1], y), x[2])) |> 
+      htmltools::tags$ol()
+    , git_libs = globalenv()$params$git_libs |>
+        purrr::map(\(x) htmltools::tags$li(
+            htmltools::tags$a(href=paste0("https://github.com/delriaan/", x)
+                              , title = paste0("delriaan/", x)
+                              , target = "_blank"
+                              , x)
+          )) |> 
+        list() |> 
+        htmltools::tags$ul()
+    )}, envir = as.environment("nb_env"))
 
 .cache <- cachem::cache_disk(dir = "r_session_cache")
+
+
+# :: Functions
+# 
+split_f <- function(x, f, ...){
+#' Formula Split
+#' 
+#' \code{split_f} is a wrapper base::split() and data.table:::split.data.table() that supports a formulaic definition of the splitting factors.
+#' 
+#' @param x See \code{?split}
+#' @param f See \code{?split}: may also be a right-hand sided formula (e.g., \code{~var1 + var2 + ...})
+#' 
+#' @return See \code{?split}
+#' 
+  if (is_formula(k)){ f <- terms(f, data = x) |> attr("term.labels") }
+  if (is.data.table(x)){ split(x, by = f, ...) } else { split(x, f = f, ...)}
+}
+
+#
+check_ndc_format <- \(lc, pc){
+#' Check and Conform NDC sequences
+#' 
+#' \code{check_ndc_format} checks labeler and product codes against a predefined, expected format and conforms the arguments accordingly.
+#' 
+#' @param lc,pc Labeler code and product code respectively (package size omitted)
+#' 
+#' @return A string in the format \code{labeler}-\code{product}
+#' 
+  pc <- modify_if(
+          pc, \(i) stri_length(i) < 3
+          , \(i) stri_pad_left(i, width = 3, pad = "0")
+          )
+  lc <- modify_if(
+          lc
+          , \(i) stri_length(i) < 4
+          , \(i) stri_pad_left(i, width = ifelse(stri_length(pc) == 3, 5, 4), pad = "0")
+          )
+  paste(lc, pc, sep = "-")
+}
