60e6e09c81cd0380e447e136272bdbcdd88a8866
Chionesu <sapienti.Dei@outlook.com>
2023-06-11 01:10:43 -0400

    Interim Commit
    Finalizing visNetwork plot

--- .SANDBOX.R
+++ .SANDBOX.R
@@ -2,12 +2,8 @@
 book.of.workflow::save_image(!!!ls(), save.dir = "rdata", file.name = "workspace", use.prefix = FALSE, use.timestamp = FALSE)
 book.of.workflow::save_image(!!!ls(nb_env), save.dir = "rdata", file.name = "nb_env", use.prefix = FALSE, use.timestamp = FALSE, envir = nb_env)
 
-plot_ly(
-  x = degree(graph = gph, mode = "in") |> make.quantiles(as.factor = !FALSE, 0:5/5)
-  , y = page_rank(graph = gph)$vector |> ratio(type = "pareto", decimals = 4)
-  , type = "box"
-  ) |>
-  plotly::layout(
-    xaxis = list(title = "In-Degree Quantile")
-    , yaxis = list(title = "Cumulative Proportional Pagerank")
-    )
+logivec <- sample(c(T,F), 4*30, TRUE) |> matrix(ncol = 4)
+apply(logivec, 1, prod) |> mean()
+
+ 
+  
